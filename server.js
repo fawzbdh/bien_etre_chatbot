e363e6e6a064c2791202278ee35a4514addcaead
StageP2 <stage2@general-industry.com>
2024-06-30 23:54:41 +0200

    Votre message de commit décrivant les modifications

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const request = require('request');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-// Configure le middleware proxy
-app.use('/dialogflow', createProxyMiddleware({
-  target: 'https://dialogflow.cloud.google.com',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/dialogflow': '/v1/integrations/messenger/webhook/882d92d7-b3d3-47a9-a973-b0b11c068ee5/sessions',
-  },
-  onProxyReq: (proxyReq, req, res) => {
-    // Ajouter des en-têtes ici si nécessaire
-    proxyReq.setHeader('Access-Control-Allow-Origin', '*');
-  }
-}));
+app.post('/dialogflow', (req, res) => {
+    const dialogflowUrl = 'https://dialogflow.cloud.google.com/v1/integrations/messenger/webhook/882d92d7-b3d3-47a9-a973-b0b11c068ee5/sessions/dfMessenger-44936424';
 
-// Démarre le serveur
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    const options = {
+        url: dialogflowUrl,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req.body),
+    };
+
+    request.post(options, (error, response, body) => {
+        if (error) {
+            console.error('Error:', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.status(response.statusCode).send(body);
+        }
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
